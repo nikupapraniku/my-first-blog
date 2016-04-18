83674efe859bdb98e198b34ded27459fd1529e7b
nikupapraniku <niku.papraniku@gmail.com>
2016-04-18 15:01:45 +0200

    Adding edit and post form

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -11,7 +12,7 @@ class Post(models.Model):
         blank=True, null=True)
 
     def publish(self):
-        self.published_date=timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
